9610d4d2791f8300a397f3976af6109ae7e9cbeb
kou <kou@railgun.(none)>
2010-10-28 16:43:16 +0900

    optimized 'env.el'.
    modified the prefix 'yas-' to 'jiros-'.
    modified 'equal' to 'eq'.
    
    Signed-off-by: kou <kou@railgun.(none)>

--- env.el
+++ env.el
@@ -8,46 +8,46 @@
 ;; OSを判定
 
 ;; GNU/Linux上で動作
-(defconst yas-on-GNU-Linux
-  (equal system-type 'gnu/linux))
+(defconst jiros-on-GNU-Linux
+  (eq system-type 'gnu/linux))
 
 ;; SunOS上で動作
-(defconst yas-on-SunOS
-  (equal system-type 'berkeley-unix))
+(defconst jiros-on-SunOS
+  (eq system-type 'berkeley-unix))
 
 ;; Solaris上で動作
-(defconst yas-on-Solaris
-  (equal system-type 'usg-unix-v))
+(defconst jiros-on-Solaris
+  (eq system-type 'usg-unix-v))
 
 ;; UNIX上で動作
-(defconst yas-on-UNIX
+(defconst jiros-on-UNIX
   (or
-   yas-on-GNU-Linux
-   yas-on-SunOS
-   yas-on-Solaris
+   jiros-on-GNU-Linux
+   jiros-on-SunOS
+   jiros-on-Solaris
    ))
 
 ;; Windows上で動作
-(defconst yas-on-Windows
-  (equal system-type 'windows-nt))
+(defconst jiros-on-Windows
+  (eq system-type 'windows-nt))
 
 ;; Mac OS X上で動作
-(defconst yas-on-MacOSX
-  (equal system-type 'darwin))
+(defconst jiros-on-MacOSX
+  (eq system-type 'darwin))
 
 ;; Mac上で動作
-(defconst yas-on-Mac
+(defconst jiros-on-Mac
   (or
-   yas-on-MacOSX
+   jiros-on-MacOSX
    ))
 
 
 ;; 判定できなかった
 (unless
     (or
-     yas-on-UNIX
-     yas-on-Windows
-     yas-on-Mac
+     jiros-on-UNIX
+     jiros-on-Windows
+     jiros-on-Mac
      )
   (message "Unknown system-type"))
 
@@ -55,35 +55,35 @@
 ;; ウィンドウシステムを判定
 
 ;; X Window System
-(defconst yas-x
-  (equal window-system 'x))
+(defconst jiros-x
+  (eq window-system 'x))
 
 ;; Meadow
-(defconst yas-w32
-  (equal window-system 'w32))
+(defconst jiros-w32
+  (eq window-system 'w32))
 
 ;; Mule for Win32
-(defconst yas-win32
-  (equal window-system 'win32))
+(defconst jiros-win32
+  (eq window-system 'win32))
 
 ;; Mac OS X
-(defconst yas-mac
-  (equal window-system 'mac))
+(defconst jiros-mac
+  (eq window-system 'mac))
 
 
 ;; 判定できなかった
 (unless
     (or
-     yas-x
-     yas-w32
-     yas-win32
-     yas-mac
+     jiros-x
+     jiros-w32
+     jiros-win32
+     jiros-mac
      )
   (message "Unknown window-system"))
 
 
 ;; Meadow
-(defconst yas-Meadow
+(defconst jiros-Meadow
   (featurep 'meadow))
 
 
