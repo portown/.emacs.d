d07bca0f96694dc8ff015af0ed357f31c9c8aef4
YASUDA Jiro <jiros@softlab.cs.tsukuba.ac.jp>
2010-12-02 09:23:59 +0900

    twittering-mode: notify new tweet
    Signed-off-by: YASUDA Jiro <jiros@softlab.cs.tsukuba.ac.jp>

--- init.d/20_twitter.el
+++ init.d/20_twitter.el
@@ -32,6 +32,16 @@
 ;; マスターパスワードを使用する
 (setq twittering-use-master-password t)
 
+;; 更新通知
+(add-hook 'twittering-new-tweets-hook
+          #'(lambda ()
+              (let ((n twittering-new-tweets-count))
+                (start-process "twittering-notify" nil "notify-send"
+                               "-i" "/usr/share/pixmaps/gnome-emacs.png"
+                               "New tweets"
+                               (format "You have %d new tweet%s"
+                                       n (if (> n 1) "s" ""))))))
+
 
 
 
