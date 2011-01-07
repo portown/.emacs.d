f5fad10a16a30e9def43c09e9148b7eb8077fa63
kou <kou@tower.(none)>
2011-01-08 00:09:43 +0900

    fix the tab size.
    Signed-off-by: kou <kou@tower.(none)>

--- init.d/01_settings.el
+++ init.d/01_settings.el
@@ -12,7 +12,7 @@
 (setq inhibit-startup-message t)
 
 ;; タブ幅を2に
-(setq-default tab-width 1)
+(setq-default tab-width 2)
 (setq-default tab-stop-list
               '(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36
                   38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68
