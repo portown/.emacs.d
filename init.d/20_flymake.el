0157eff56580968ee3cbab448e7feb6d91b8718b
kou <kou@tower.(none)>
2011-01-23 00:29:29 +0900

    add flymake.
    flymakeを導入。
    
    Signed-off-by: kou <kou@tower.(none)>

---
+++ init.d/20_flymake.el
@@ -0,0 +1,15 @@
+;;; 20_flymake.el
+;;; Copyright (C) 2011 YASUDA Jiro
+
+;;; Author: YASUDA Jiro
+;;; Time:   '11/01/23
+
+
+(require 'flymake)
+
+
+(set-face-background 'flymake-errline "red4")
+(set-face-background 'flymake-warnline "dark slate blue")
+
+
+;; EOF
