ca9892fa6dcf7710b29024e46deeb13ed3bf1c21
kou <kou@tower.(none)>
2011-01-23 00:29:53 +0900

    add flymake on ruby.
    Ruby用flymake設定を追加。
    
    Signed-off-by: kou <kou@tower.(none)>

--- init.d/30_ruby.el
+++ init.d/30_ruby.el
@@ -10,6 +10,18 @@
 (require 'ruby-electric)
 
 
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+
 (setq auto-mode-alist
       (append
        (list
@@ -28,6 +40,10 @@
               (require 'ruby-block)
               (ruby-block-mode t)
               (setq ruby-block-highlight-toggle t)
+
+              ;; Don't want flymake mode for ruby regions in rhtml files
+              (if (not (null buffer-file-name))
+                  (flymake-mode))
               ))
 
 
