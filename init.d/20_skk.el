b7520e902e987c65acc14e614b23c57de0d4b568
YASUDA Jiro <jiros@softlab.cs.tsukuba.ac.jp>
2011-02-03 13:18:55 +0900

    devide 20_skk.el into 20_skk.el and skk.el.
    Signed-off-by: YASUDA Jiro <jiros@softlab.cs.tsukuba.ac.jp>

--- init.d/20_skk.el
+++ init.d/20_skk.el
@@ -4,144 +4,29 @@
 (require 'skk-autoloads)
 
 
-(defvar jiros-setting-use-skk-server nil
-  "Whether you use the skk server. t is to use.")
+;; SKKの初期設定ファイル名
+(setq skk-init-file "~/.emacs.d/settings/skk.el")
 
-(defvar jiros-setting-skk-server-host "localhost"
-  "The hostname of skk server.")
-
-(defvar jiros-setting-skk-server-portnum 1178
-  "The port number of skk server.")
-
-(defvar jiros-setting-skk-large-jisyo "/usr/share/skk/SKK-JISYO.L"
-  "The path of skk large-jisyo file.")
+;; SKKの初期設定ファイルをバイトコンパイルするか否か
+(setq skk-byte-compile-init-file t)
 
 
-;; ツールチップを表示する
-(setq skk-show-tooltip t)
+;; isearchでSKKを使えるようにする設定
 
-;; インライン表示する(よく分からん
-(setq skk-show-inline t)
-
-;; SKKのメッセージを日本語で表示しない
-(setq skk-japanese-message-and-error nil)
-
-;; SKKのチュートリアル
-(setq skk-tut-file "/usr/share/skk/SKK.tut")
-
-;; 動的補完を行う
-(setq skk-dcomp-activate t)
-
-;; 接頭辞・接尾辞開始文字の設定
-(setq skk-special-midashi-char-list '(?>))
-
-
-;; ↓サーバの設定↓
-(if jiros-setting-use-skk-server
-    (progn
-      ;; 大規模辞書はなし
-      (setq skk-large-jisyo nil)
-
-      ;; サーバホスト
-      (setq skk-server-host jiros-setting-skk-server-host)
-
-      ;; サーバのポート番号
-      (setq skk-server-portnum jiros-setting-skk-server-portnum)
-
-      (setq skk-preload nil)         ; 辞書をEmacs起動時にロードしない
-      )
-  ;; ↑サーバの設定↑
-
-  (setq skk-large-jisyo jiros-setting-skk-large-jisyo)
-  (setq skk-preload t)               ; 辞書をEmacs起動時にロードする
-  )
-
-
-;; SKKのモードラインに表示される文字を設定
-(setq skk-latin-mode-string "a")
-(setq skk-hiragana-mode-string "あ")
-(setq skk-katakana-mode-string "ア")
-(setq skk-jisx0201-mode-string "ｱ")
-(setq skk-jisx0208-latin-mode-string "ａ")
-
-;; 半角カナの入力を許可する
-(setq skk-use-jisx0201-input-method t)
-
-;; SKK起動時のフック
-(add-hook 'skk-mode-hook
-          #'(lambda ()
-              (ccc-setup)
-              (define-key skk-j-mode-map "\C-Q" 'skk-jisx0201-mode)
-              ))
-
-;; isearchでSKKを使えるように
+;; isearch開始時設定
 (add-hook 'isearch-mode-hook
           #'(lambda ()
               (when (and (boundp 'skk-mode)
                          skk-mode
                          skk-isearch-mode-enable)
                 (skk-isearch-mode-setup))))
+
+;; isearch終了時設定
 (add-hook 'isearch-mode-end-hook
           #'(lambda ()
               (when (and (featurep 'skk-isearch)
                          skk-isearch-mode-enable)
                 (skk-isearch-mode-cleanup))))
-
-;; ;; ローマ字ルールの変更
-;; (setq skk-rom-kana-rule-list
-;;       '(
-;;         ;; ("si"  nil ("スィ" . "すぃ"))
-;;         ;; ("ti"  nil ("ティ" . "てぃ"))
-;;         ;; ("tu"  nil ("トゥ" . "とぅ"))
-;;         ;; ("dwo" nil ("ドゥ" . "どぅ"))
-;;         ;; ("hh"  "h" ("ッ"   . "っ"))
-;;         ;; ("wi"  nil ("ヰ"   . "ゐ"))
-;;         ;; ("whi" nil ("ウィ" . "うぃ"))
-;;         ;; ("we"  nil ("ヱ"   . "ゑ"))
-;;         ;; ("whe" nil ("ウェ" . "うぇ"))
-;;         ;; ("who" nil ("ウォ" . "うぉ"))
-;;         ;; ("kwa" nil ("クァ" . "くぁ"))
-;;         ;; ("kwi" nil ("クィ" . "くぃ"))
-;;         ;; ("kwe" nil ("クェ" . "くぇ"))
-;;         ;; ("kwo" nil ("クォ" . "くぉ"))
-;;         ;; ("tsa" nil ("ツァ" . "つぁ"))
-;;         ;; ("tsi" nil ("ツィ" . "つぃ"))
-;;         ;; ("tse" nil ("ツェ" . "つぇ"))
-;;         ;; ("tso" nil ("ツォ" . "つぉ"))
-;;         (" " nil ("　" . "　"))
-;;         ("!" nil ("！" . "！"))
-;;         ("(" nil ("（" . "（"))
-;;         (")" nil ("）" . "）"))
-;;         ))
-
-;; 句読点リストの設定
-(setq-default skk-kutouten-type 'jp)
-
-;; 変換候補選択キーの設定
-(setq skk-henkan-show-candidates-keys '(?a ?o ?e ?u ?h ?t ?n))
-
-;; ;; ACTの使用
-;; (setq skk-use-act t)
-
-;; ;; NICOLAの使用
-;; (setq skk-use-kana-keyboard t)
-;; (setq skk-kanagaki-keyboard-type 'nicola-dvorak)
-
-;; ;; モードラインに表示される文字
-;; (setq skk-nicola-hiragana-mode-string "い")
-;; (setq skk-nicola-katakana-mode-string "イ")
-
-;; ;; 親指シフト認識時間
-;; (setq skk-nicola-interval 0.08)
-
-;; ;; 左親指キー
-;; (setq skk-nicola-lshift-keys '([muhenkan]))
-
-;; ;; 右親指キー
-;; (setq skk-nicola-rshift-keys '([henkan]))
-
-
-
 
 
 ;; EOF
---
+++ settings/skk.el
@@ -0,0 +1,140 @@
+;;; skk.el
+;;; Copyright (C) 2011 YASUDA Jiro
+
+;;; Author: YASUDA Jiro
+;;; Time:   '11/02/03
+
+
+;; ;; ACT用設定
+
+;; (setq skk-use-act t)
+
+
+;; ;; NICOLA用設定
+
+;; (setq skk-use-kana-keyboard t)
+;; (setq skk-kanagaki-keyboard-type 'nicola-dvorak)
+
+;; ;; モードラインに表示される文字
+;; (setq skk-nicola-hiragana-mode-string "い")
+;; (setq skk-nicola-katakana-mode-string "イ")
+
+;; ;; 親指シフト認識時間
+;; (setq skk-nicola-interval 0.08)
+
+;; ;; 左親指キー
+;; (setq skk-nicola-lshift-keys '([muhenkan]))
+
+;; ;; 右親指キー
+;; (setq skk-nicola-rshift-keys '([henkan]))
+
+
+;; 月配列(Ux)-Dvorak用設定
+
+(load "skk-tsukiux-dvorak")
+
+
+;; その他の設定
+
+
+;; SKKのメッセージを日本語で表示するか否か
+(setq skk-japanese-message-and-error nil)
+
+
+;; SKKのチュートリアルファイルのパス
+(setq skk-tut-file "/usr/share/skk/SKK.tut")
+
+
+;; 動的補完を行うか否か
+(setq skk-dcomp-activate t)
+
+
+;; ;; 接頭辞・接尾辞開始文字の設定
+;; (setq skk-special-midashi-char-list '(?>))
+
+
+;; 半角カナの入力を許可するか否か
+(setq skk-use-jisx0201-input-method t)
+
+
+;; 句読点リストの設定
+(setq-default skk-kutouten-type 'jp)
+
+
+;; 変換候補選択キーの設定
+;; Dvorak仕様
+(setq skk-henkan-show-candidates-keys '(?a ?o ?e ?u ?h ?t ?n))
+
+
+;; 変換候補の表示方法の設定
+;; 変換候補表示方法は以下の3つ
+;;   エコーエリア：見やすいがミニバッファ内で変換しようとするとコケる
+;;   ツールチップ：環境によると思われるが見づらい
+;;   インライン　：文章中に現れるためレイアウトが一時的に崩れる
+;; ツールチップとインライン両方設定するとインライン優先のよう
+
+(setq skk-show-tooltip nil)
+(setq skk-show-inline t)
+
+
+;; SKKのモードラインに表示される文字を設定
+(setq skk-latin-mode-string "a")
+(setq skk-hiragana-mode-string "あ")
+(setq skk-katakana-mode-string "ア")
+(setq skk-jisx0201-mode-string "ｱ")
+(setq skk-jisx0208-latin-mode-string "ａ")
+
+
+;; 辞書設定
+
+;; 辞書サーバを使う設定
+(setq skk-large-jisyo nil)
+(setq skk-server-host "127.0.0.1")
+(setq skk-server-portnum 1178)
+(setq skk-preload nil)
+
+;; ;; 辞書サーバを使わない設定
+;; (setq skk-large-jisyo "/usr/share/skk/SKK-JISYO.L")
+;; (setq skk-server-host nil)
+;; (setq skk-server-portnum nil)
+;; (setq skk-preload t)
+
+
+;; SKK起動時のフック
+(add-hook 'skk-mode-hook
+          #'(lambda ()
+              (ccc-setup)
+              (define-key skk-j-mode-map "\C-Q" 'skk-jisx0201-mode)
+              ))
+
+
+;; ;; ローマ字ルールの変更
+;; ;; 現在は月配列なので使用せず
+;; (setq skk-rom-kana-rule-list
+;;       '(
+;;         ("si"  nil ("スィ" . "すぃ"))
+;;         ("ti"  nil ("ティ" . "てぃ"))
+;;         ("tu"  nil ("トゥ" . "とぅ"))
+;;         ("dwo" nil ("ドゥ" . "どぅ"))
+;;         ("hh"  "h" ("ッ"   . "っ"))
+;;         ("wi"  nil ("ヰ"   . "ゐ"))
+;;         ("whi" nil ("ウィ" . "うぃ"))
+;;         ("we"  nil ("ヱ"   . "ゑ"))
+;;         ("whe" nil ("ウェ" . "うぇ"))
+;;         ("who" nil ("ウォ" . "うぉ"))
+;;         ("kwa" nil ("クァ" . "くぁ"))
+;;         ("kwi" nil ("クィ" . "くぃ"))
+;;         ("kwe" nil ("クェ" . "くぇ"))
+;;         ("kwo" nil ("クォ" . "くぉ"))
+;;         ("tsa" nil ("ツァ" . "つぁ"))
+;;         ("tsi" nil ("ツィ" . "つぃ"))
+;;         ("tse" nil ("ツェ" . "つぇ"))
+;;         ("tso" nil ("ツォ" . "つぉ"))
+;;         (" " nil ("　" . "　"))
+;;         ("!" nil ("！" . "！"))
+;;         ("(" nil ("（" . "（"))
+;;         (")" nil ("）" . "）"))
+;;         ))
+
+
+;; EOF
